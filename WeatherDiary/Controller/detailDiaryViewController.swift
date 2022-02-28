5721a068b96bb65887cb4d00cb75c8955be0d5a1
m.t <topredswin2@yahoo.co.jp>
2022-02-28 11:37:52 +0900

    画面ごとのViewController.swiftファイルの作成しディレクトリに分ける #3

---
+++ WeatherDiary/Controller/detailDiaryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  detailDiaryViewController.swift
+//  WeatherDiary
+//
+//  Created by 戸井田莉江 on 2022/02/28.
+//
+
+import UIKit
+
+class detailDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
