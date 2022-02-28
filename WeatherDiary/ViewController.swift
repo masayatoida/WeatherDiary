c728a7465efc831a5fd5fd22300a2d02594ba0f5
m.t <topredswin2@yahoo.co.jp>
2022-02-28 10:32:07 +0900

    Initial Commit

---
+++ WeatherDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherDiary
+//
+//  Created by 戸井田莉江 on 2022/02/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
