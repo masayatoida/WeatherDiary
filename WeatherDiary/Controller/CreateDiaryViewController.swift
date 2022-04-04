e2e449b1dc602ae449ce9d96da4228be8d092ed1
m.t <topredswin2@yahoo.co.jp>
2022-04-04 20:35:41 +0900

    日記新規作成画面のレイアウト作成

--- WeatherDiary/Controller/CreateDiaryViewController.swift
+++ WeatherDiary/Controller/CreateDiaryViewController.swift
@@ -9,7 +9,30 @@ import UIKit
 
 class CreateDiaryViewController: UIViewController {
     
+    @IBOutlet private weak var eventTextfield: UITextField!
+    
+    var date = Date()
+    private let diaryData = DiaryData()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let df =  DateFormatter()
+        df.dateFormat = "yyyy/MM/dd"
+        navigationItem.title = df.string(from: date)
+        diaryData.allData()
+    }
+    
+    @IBAction func didTapSave(_ sender: UIButton) {
+        saveDate()
+        navigationController?.popViewController(animated: true)
+    }
+    @IBAction func didTapDelete(_ sender: UIButton) {
+    }
+    
+    private func saveDate() {
+        let df =  DateFormatter()
+        df.dateFormat = "yyyy/MM/dd"
+        diaryData.saveData(date: df.string(from: date), event: eventTextfield.text ?? "")
+        diaryData.allData()
     }
 }
